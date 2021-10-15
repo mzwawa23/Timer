7262f517f5b93894959bd0aee94ce5cced1a0f88
mzwawa23 <mzwawa@parasoft.com>
2021-10-15 13:36:45 +0200

    modifications for local changes testing

--- clock.c
+++ clock.c
@@ -7,6 +7,48 @@
 #include "consts.h"
 #include "inout.h"
 
+static const int VERY_LOW = 1;
+static const int LOW = 2;
+static const int MEDIUM = 3;
+static const int HIGH = 4;
+static const int VERY_HIGH = 5;
+
+static int value = 0;
+
+static int power(int base, int exp)
+{
+    value = base;
+    int power_value = -1;
+    int i = 1;
+    while (i<=exp && i>0)
+    {
+    	i++;
+        value = value * base;
+    }
+
+    if (value <= 10)
+    {
+		power_value = VERY_LOW;
+    }
+     if (value > 10 && value <= 100)
+    {
+		power_value = LOW;
+    }
+    else if (value > 100 && value <= 5000)
+    {
+		power_value = MEDIUM;
+    }
+    else if (value > 5000 && value <= 50000)
+    {
+		power_value = HIGH;
+    }
+    else if (value > 50000)
+    {
+		power_value = VERY_HIGH;
+    }
+  return power_value;
+}
+
 /*
  * Print the current time
  */
